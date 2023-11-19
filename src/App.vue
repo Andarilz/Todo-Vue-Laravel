f06d2755fbd17467ee85a9e20421cc709f07248a
Andarilz <isgrammar@gmail.com>
2023-11-20 00:47:19 +0300

    Create static todo page

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Navbar from './components/Navbar.vue'
+import TodosPage from './pages/TodosPage.vue'
+import Footer from "./components/Footer.vue"
+
+</script>
+
+<template>
+
+  <Navbar />
+
+  <TodosPage />
+
+  <Footer />
+
+</template>
+
+<style scoped>
+
+</style>
