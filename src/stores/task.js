0bf55562e09bc0b00684e9f139e751e9bb67efe9
Andarilz <isgrammar@gmail.com>
2023-11-26 21:17:52 +0300

    Store rewrite to setup

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia"
 import { allTasks } from "../http/task-api"
+import { ref, reactive, computed } from "vue"
 
-export const useTaskStore = defineStore("taskStore", {
+const tmp = {
 	state: () => ({
 		tasks: [],
 	}),
@@ -16,4 +17,30 @@ export const useTaskStore = defineStore("taskStore", {
 			this.tasks = data.data
 		}
 	}
+}
+
+export const useTaskStore = defineStore("taskStore", () => {
+	const tasks = ref([])
+	// const task  = variable({
+	// 	id: null,
+	// 	name: null,
+	// 	is_completed: false
+	// })
+
+	const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+	const completedTasks   = computed(() => tasks.value.filter(task =>  task.is_completed))
+
+	const fetchAllTasks = async () => {
+		const { data } = await allTasks()
+		tasks.value = data.data
+	}
+
+	return {
+		tasks,
+		uncompletedTasks,
+		completedTasks,
+		fetchAllTasks
+	}
+
 })
+
