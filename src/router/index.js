7dfedb0cec4c639e462a90e5c8d8cf2078313f27
Andarilz <isgrammar@gmail.com>
2023-11-29 00:54:59 +0300

    Guard add

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes"
 
-export default createRouter({
+const router = createRouter({
 	routes,
 	history: createWebHistory(),
 	// linkActiveClass: "active"
 })
+
+router.beforeEach((to, from) => {
+	if(to.path == "/login"){
+		return { name: "register" }
+	}
+})
+
+export default router
